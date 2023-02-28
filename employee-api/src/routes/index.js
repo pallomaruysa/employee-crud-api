8dedc85d950465885f5c45635121bab84c7ad113
pallomaruysa <pallomaruysa@gmail.com>
2023-02-28 04:56:23 -0300

    Desenvolvimento dos arquivos para a execucao da api

--- employee-api/src/routes/index.js
+++ employee-api/src/routes/index.js
@@ -4,3 +4,17 @@
  * date: 02/25/2023
  * author: Palloma Ruysa
  */
+
+const express= require('express');
+
+const router = express.Router();
+
+router.get('/api', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'Seja bem-vindo(a) a API Node.js + PostgreSQL + Azure',
+    version: '1.0.0'
+  });
+});
+
+module.exports = router;
